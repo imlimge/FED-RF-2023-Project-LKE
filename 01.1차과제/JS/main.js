a2c11505145f778f3023f82da6dcdbc113f807d4
imlimge <limge12@gmail.com>
2023-08-28 09:37:23 +0900

    230828_0937

---
+++ 01.1차과제/JS/main.js
@@ -0,0 +1,22 @@
+/* 버거킹 : 메인 JS - main.js */
+
+
+// 변경대상 : banner_burgerImg
+// var burgerImg = document.querySelector('.banner_burgerImg');
+
+// console.log("배너버거이미지",burgerImg);
+
+
+// var hcode = '';
+
+
+// for(var i=0;i<9;i++){
+//   hcode += `
+//    <img src="./images/03.main/01.banner/burger_img (${i+1}).png" alt="배너버거이미지${i+1}">
+
+
+//   `;
+
+// }////// for //////
+
+// burgerImg.innerHTML = hcode;
