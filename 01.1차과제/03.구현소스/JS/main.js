f80ef04bf86c8bf22d1097f541b1f514680b0eef
imlimge <limge12@gmail.com>
2023-09-05 18:20:39 +0900

    230905_1820

--- 01.1차과제/03.구현소스/JS/main.js
+++ 01.1차과제/03.구현소스/JS/main.js
@@ -1,10 +1,77 @@
 /* 버거킹 : 메인 JS - main.js */
 
 
+
+window.addEventListener('DOMContentLoaded', loadFn);
+
 //DOM선택함수
-// const qs = function(x){return document.querySelector(x)}; 아래의 원형
 const qs = (x) => document.querySelector(x);
 const qsa = (x) => document.querySelectorAll(x);
+
+
+
+
+function loadFn(){
+   
+
+    // 움직일 대상: .mover
+    const mover = qs('.mover');
+
+    // 이벤트 대상: .banner
+    const banner = qs('.banner');
+
+   
+    let gap = mover.clientWidth/2;
+
+    // 무버가 따라다니는 기능 구현
+    banner.onmousemove = e => {
+
+        let posx = e.pageX - gap;
+        let posy = e.pageY - gap;
+        mover.style.top = posy + 'px';
+        mover.style.left = posx + 'px';
+
+    };//onmousemove /////////
+
+
+  // 이벤트 구역을 들어올때만 보이기 / 나가면 숨기기
+  banner.onmouseenter = () =>{mover.style.opacity = 1;}; /// mouseenter////
+  banner.onmouseleave = () =>{mover.style.opacity = 0;}; /// mouseleave////
+
+
+
+
+  //버거이미지 오버시 원 크게 만들기
+    const burger = qs('.banner-burgerImg');
+    const moverLetter = qsa('.mover span');
+
+    console.log(moverLetter);
+    
+    burger.onmouseenter = () => {
+        mover.style.width = '140px';
+        mover.style.height = '140px';
+        moverLetter.style.opacity = '100%';
+       
+    };
+
+    burger.onmouseleave = () => {
+        mover.style.width = '20px';
+        mover.style.height = '20px';
+    
+        moverLetter.style.opacity = '0%';
+    };
+
+
+
+ 
+
+
+
+
+
+    
+
+} ///////loadFn 함수//////////////////
 
 
 
