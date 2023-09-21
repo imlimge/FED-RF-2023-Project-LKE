5e9e49daf9e4c431f91cac62bf5841fcacd2db48
imlimge <limge12@gmail.com>
2023-09-21 14:43:41 +0900

    230921_1443

--- 01.1차과제/03.구현소스/JS/sub01.js
+++ 01.1차과제/03.구현소스/JS/sub01.js
@@ -35,7 +35,7 @@
       `<li>
       <figure>
           <img src="./images/04-1.sub01-menu/01.whats-new/menulist/menu_new${i}.png" alt="${bData[i-1]}">
-          <figcaption>${bData[i-1]}</figcaption>
+          <figcaption><p>${bData[i-1]}</p></figcaption>
       </figure>
       </li>
       `
@@ -44,7 +44,7 @@
     
     hcode += '</ul>';
 
-   console.log(hcode);
+    //console.log(hcode);
 
     burger.innerHTML = hcode;
 
@@ -53,4 +53,77 @@
 
 
 
+
+
+/**************************************  
+  서브페이지 - 메뉴리스트 슬라이드 구현
+**************************************/
+// 이벤트 대상
+const mbtn = dFn.qsa('.abtn');
+
+// 변경 대상
+const mList = dFn.qs('.menu-list');
+
+console.log(mbtn,mList);
+
+
+mbtn.forEach(ele=>dFn.addEvt(ele,'click',bMove));
+
+
+
+function bMove(){
+
+  
+for (let i = 25; i<100; i += 25){
+ 
+
+
+  
+
+  let isRight = this.classList.contains('ab2');
+ 
+  console.log(this,isRight);
+
+ if(isRight){
+   
+   mList.style.right = i+'%';
+   console.log(i);
+    
+   }
+
+   else{
+     mList.style.right = -i+'%';
+   }
+
+
+}
+}
+
+
+
+/* 
+function bMove(){
+
+  for (let i = 25; i<100; i += 25){
+        
+            let isRight = this.classList.contains('ab2');
+          
+            console.log(this,isRight);
+      
+          if(isRight){
+            
+                      
+            mList.style.right = i+'%';
+            console.log(i);
+                      
+          
+
+            }
+
+            else{
+              mList.style.right = -i+'%';
+            }
+      }  
+}
+ */
 
