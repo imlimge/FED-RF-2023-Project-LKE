a092703bc849e94bffd8120465b7dcf9a75b9363
Tom B. Parker <tombap8@hotmail.com>
2023-09-22 10:38:44 +0900

    ss

--- 01.1차과제/03.구현소스/JS/sub01.js
+++ 01.1차과제/03.구현소스/JS/sub01.js
@@ -1,15 +1,8 @@
 /* 버거킹 : 서브 JS - sub01.js */
 
-
-
-  //모듈 불러오기////////////
-  //Dom함수 모듈
-  import dFn from './dom.js';
-
-
-
-
-
+//모듈 불러오기////////////
+//Dom함수 모듈
+import dFn from "./dom.js";
 
 /**************************************  
   서브페이지 - 메뉴
@@ -17,81 +10,104 @@
 
 **************************************/
 
+// 메뉴 대상 요소 : .burger
+const burger = dFn.qs(".burger");
+//console.log(burger);
+const menuFolder = {
+  "스페셜&할인팩":"01.whats-new",
+  "신제품(NEW)":"02.breakfast",
+}
 
-  // 메뉴 대상 요소 : .burger
-  const burger = dFn.qs(".burger");
-  //console.log(burger);
+// bData.forEach(val,idx)
 
- 
-  // bData.forEach(val,idx)
+// 최초 메뉴리스트 만들기 함수 호출
+changeMenu("스페셜&할인팩");
+
+
+//// 메뉴리스트 변경 함수 /////////
+function changeMenu(menu){
+  const selData = mData.메뉴소개[menu];
+  const CNT_SEL = selData.length;
+  console.log(selData);
   
-
+  let hcode = "<ul>";
   
-      let hcode = '<ul>';
+  for (let i = 1; i <= CNT_SEL; i++) {
+    hcode += `<li>
+        <figure>
+            <img src="./images/04-1.sub01-menu/01.whats-new/menulist/menu_new${i}.png" alt="${
+      bData[i - 1]
+    }">
+            <figcaption><p>${selData[i]}</p></figcaption>
+        </figure>
+        </li>
+        `;
+    // hcode += `<li>
+    //     <figure>
+    //         <img src="./images/04-1.sub01-menu/${menuFolder[menu]}/menulist/menu_new${i}.png" alt="${
+    //   bData[i - 1]
+    // }">
+    //         <figcaption><p>${selData[i]}</p></figcaption>
+    //     </figure>
+    //     </li>
+    //     `;
+  }
+  
+  hcode += "</ul>";
+  
+  //console.log(hcode);
+  
+  burger.innerHTML = hcode;
 
-      for(let i=1; i<=18; i++){
+} ///////// changeMenu 함수 ///////////
 
-      hcode +=
-      `<li>
-      <figure>
-          <img src="./images/04-1.sub01-menu/01.whats-new/menulist/menu_new${i}.png" alt="${bData[i-1]}">
-          <figcaption><p>${Object.keys("스페셜&할인팩"[i-1])}</p></figcaption>
-      </figure>
-      </li>
-      `
-      ;
-      }
-    
-    hcode += '</ul>';
+//// 메뉴 클릭 설정만들기 ///////////
+const mListItem = dFn.qsa('.menu-list-item');
 
-    //console.log(hcode);
-
-    burger.innerHTML = hcode;
+mListItem.forEach(ele=>{
+  ele.onclick = ()=>{
+    // 하위 h4요소 글자 읽기
+    let txt = dFn.qsEl(ele,'h4').innerText;
+    console.log(txt);
+    changeMenu(txt);
+  }
+})
 
 
- /*    bData[i-1] */
-
-
-
-
+/*    bData[i-1] */
 
 /**************************************  
   서브페이지 - 메뉴리스트 슬라이드 구현
 **************************************/
 // 이벤트 대상
-const mbtn = dFn.qsa('.abtn');
+const mbtn = dFn.qsa(".abtn");
 
 // 변경 대상
-const mList = dFn.qs('.menu-list');
-
-console.log(mbtn,mList);
-
-
-mbtn.forEach(ele=>dFn.addEvt(ele,'click',bMove));
+const mList = dFn.qs(".menu-list");
+// 이동순번
+let seqNum = 0;
 
 
+mList.style.transition = '2s';
 
-function bMove(){
+console.log(mbtn, mList);
 
-   
+mbtn.forEach((ele) => dFn.addEvt(ele, "click", bMove));
 
-  let isRight = this.classList.contains('ab2');
- 
-  console.log(this,isRight);
+function bMove() {
+  let isRight = this.classList.contains("ab2");
 
- if(isRight){
-   
-   mList.style.right = '25%';
-   
-   }
+  console.log(this, isRight);
 
-   else{
-     mList.style.right = '0%';
-   }
-
-
+  if (isRight) {
+    seqNum++;
+    if(seqNum>5)seqNum=5;
+  } else {
+    seqNum--;
+    if(seqNum<0)seqNum=0;
+  }
+  mList.style.right = seqNum * 15 + "%";
 }
-
 
 /* 
 function bMove(){
@@ -118,4 +134,3 @@ function bMove(){
       }  
 }
  */
-
