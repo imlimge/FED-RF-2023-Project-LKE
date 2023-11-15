5d9302cf742655a2e34d5210aa5c314819bf8c01
imlimge <limge12@gmail.com>
2023-11-15 18:19:15 +0900

    231115_1819

--- 02.2차과제/03.구현소스/js/main.js
+++ 02.2차과제/03.구현소스/js/main.js
@@ -2,44 +2,102 @@
 //Dom함수 모듈
 import dFn from "./dom.js";
 
-
-
-
 /*------------------------- 네비게이션 -  on ------------------------ */
 
-/***************************************************************  
- * 
+/***************************************************************
+ *
  *  기능 : 네비게이션 항목 해당 페이지 위치되면 on 변경
- *  
-***************************************************************/
+ *
+ ***************************************************************/
+
+// 네비게이션 아이템들 (on)
+/* const gnbItem = dFn.qsa('.gnb__item');
+console.log(gnbItem);
+
+var winY = window.scrollY;
 
 
+ const introP = dFn.qs('#intro').offsetTop;
+const aboutP = dFn.qs('#about').offsetTop;
+const facilityP = dFn.qs('#facility').offsetTop;
+const timeTableP = dFn.qs('#time_table').offsetTop;
+const noticeP = dFn.qs('#notice').offsetTop;
+const contactP = dFn.qs('#contact').offsetTop;
 
+ 
+console.log(introP,aboutP,facilityP,timeTableP,noticeP,contactP)
 
+if(winY>aboutP){
+  gnbItem.addClass('on');
+}  */
 
+// 제이쿼리로 시도
+const gnbMenu = $(".gnb__menu");
+const mGnbMenu = $(".m-gnb__menu");
+console.log(gnbMenu);
 
+const introP = $("#intro").offset().top;
+const aboutP = $("#about").offset().top;
+const facilityP = $("#facility").offset().top;
+const timeTableP = $("#time_table").offset().top;
+const noticeP = $("#notice").offset().top;
+const contactP = $("#contact").offset().top;
 
+console.log(introP,'/', aboutP,'/', facilityP,'/', timeTableP,'/', noticeP,'/', contactP);
 
+const bumwee = [
+  [introP, aboutP],
+  [aboutP, facilityP],
+  [facilityP, timeTableP],
+  [timeTableP, noticeP],
+  [noticeP, contactP],
+  [contactP, contactP+1000],
+];
+
+const addOn = (seq, scTop, start, end) => {
+  if (scTop > start && scTop < end) {
+    gnbMenu
+      .find(".gnb__item").eq(seq)
+      .addClass("on")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("on");
+
+    mGnbMenu
+      .find(".m-gnb__item").eq(seq)
+      .addClass("on")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("on");
+
+   
+
+      
+  }
+};
+
+$(window).scroll(() => {
+  let scTop = $(window).scrollTop();
+   console.log(scTop);
+
+  for (let x = 0; x < 6; x++) 
+    addOn(x, scTop, bumwee[x][0], bumwee[x][1]);
+});
 
 /*------------------------------- 인트로 ------------------------------- */
-/***************************************************************  
- * 
+/***************************************************************
+ *
  *  기능 : 인트로 로고 스크롤 시 회전하며 움직임
- *  
-***************************************************************/
+ *
+ ***************************************************************/
 
-
-
-
-
-
-
-
-/***************************************************************  
- * 
+/***************************************************************
+ *
  *  기능 : 인트로 라운드 박스 원형에 작은 원형 회전 애니메이션
- *  
-***************************************************************/
+ *
+ ***************************************************************/
 
 // 대상: .tg
 const tg = dFn.qs(".small-round");
@@ -51,14 +109,10 @@ let TM = 1000;
 // 회전함수 최초호출
 rotFn();
 
-
-
-tg2.forEach(ele => {
- rotFn2(ele);
+tg2.forEach((ele) => {
+  rotFn2(ele);
   // console.log(ele);
-}); 
-
-
+});
 
 function rotFn() {
   setTimeout(() => {
@@ -112,11 +166,7 @@ function rotFn() {
   setTimeout(rotFn, TM * 8);
 } //////rotFn 함수/////
 
-
-
-
 function rotFn2(ele) {
-
   setTimeout(() => {
     ele.style.top = "0";
     ele.style.left = "50%";
@@ -139,53 +189,43 @@ function rotFn2(ele) {
     ele.style.transition = `top ${TM}ms ease-in, left ${TM}ms ease-out`;
   }, TM * 3);
 
-  setTimeout(()=>rotFn2(ele), TM * 4);
-
+  setTimeout(() => rotFn2(ele), TM * 4);
 } ////// rotFn2 함수 ///////
 
-
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
   // window resize시 처리
   let winW = window.innerWidth;
-  console.log(winW)
-})
+});
 
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+    },
+  },
+});
 
-
-
-    var swiper2 = new Swiper(".mySwiper2", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      centeredSlides: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1.2, 
-          spaceBetween: 10,
-        },
-        500: {
-          slidesPerView: 1.5, 
-          spaceBetween: 10,
-        },
-        900: {
-          slidesPerView: 2.5, 
-          spaceBetween: 20,
-        },
-        1000: {
-          slidesPerView: 3.5, 
-          spaceBetween: 20,
-        },
-    
-    }
-    });
-
-
-var swiper = new Swiper(".mySwiper",
-{
-
+var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
@@ -203,27 +243,21 @@ var swiper = new Swiper(".mySwiper",
 
   breakpoints: {
     300: {
-      slidesPerView: 1.3, 
+      slidesPerView: 1.3,
       spaceBetween: 1,
     },
     700: {
-      slidesPerView: 2, 
+      slidesPerView: 2,
       spaceBetween: 20,
     },
     1000: {
-      slidesPerView: 2.2, 
+      slidesPerView: 2.2,
       spaceBetween: 20,
     },
-
-}
-
-}
-
-);
-
-
+  },
+});
 
 /* _____________________  공지사항  _________________________ */
-$( function() {
-  $( ".accordion" ).accordion();
-} );
+$(function () {
+  $(".accordion").accordion();
+});
