8c8ef354f84ab5661837832a021d089e07696f1d
imlimge <limge12@gmail.com>
2023-12-08 12:38:13 +0900

    231208_1238

--- 02.2차과제/03.구현소스/kairos-app/src/data/photo.js
+++ 02.2차과제/03.구현소스/kairos-app/src/data/photo.js
@@ -10,21 +10,21 @@ export const photoData = {
 
 혁신관:
 [
-  {idx:1, tit:"학원 전경", isrc:"images/facility/siteH/photo1.png"},
-  {idx:2, tit:"자습실", isrc:"images/facility/siteH/photo2.png"},
-  {idx:3, tit:"카페", isrc:"images/facility/siteH/photo3.png"},
-  {idx:4, tit:"자습실 전경", isrc:"images/facility/siteH/photo4.png"},
-  {idx:5, tit:"강의실 1", isrc:"images/facility/siteH/photo5.png"},
-  {idx:6, tit:"강의실 2", isrc:"images/facility/siteH/photo6.png"},
+  {idx:1, tit:"학원 전경", isrc:"./images/facility/siteH/aa.png"},
+  {idx:2, tit:"자습실", isrc:"./images/facility/siteH/photo2.png"},
+  {idx:3, tit:"카페", isrc:"./images/facility/siteH/photo3.png"},
+  {idx:4, tit:"자습실 전경", isrc:"./images/facility/siteH/photo4.png"},
+  {idx:5, tit:"강의실 1", isrc:"./images/facility/siteH/photo5.png"},
+  {idx:6, tit:"강의실 2", isrc:"./images/facility/siteH/photo6.png"},
 ]
 
   ,
 
 
 단계관:[
-    {idx:1, tit:"학원 전경", isrc:"images/facility/siteD/photo1.png"},
-    {idx:2, tit:"자습실", isrc:"images/facility/siteD/photo2.png"},
-    {idx:3, tit:"강의실1", isrc:"images/facility/siteD/photo3.png"},
+    {idx:1, tit:"학원 전경", isrc:"./images/facility/siteD/photo1.png"},
+    {idx:2, tit:"자습실", isrc:"./images/facility/siteD/photo2.png"},
+    {idx:3, tit:"강의실1", isrc:"./images/facility/siteD/photo3.png"},
  
   ]
 
